de728c5b098dcb0835bb5eb8d3fd3bc210a3f049
John Grosh <john.a.grosh@gmail.com>
2019-09-16 18:09:10 -0400

    changed premium from 4 to 5 weeks max length

--- src/main/java/com/jagrosh/giveawaybot/entities/PremiumLevel.java
+++ src/main/java/com/jagrosh/giveawaybot/entities/PremiumLevel.java
@@ -24,9 +24,9 @@ import com.jagrosh.giveawaybot.Constants;
 public enum PremiumLevel
     {
         NONE   (0, "None",          60*60*24*7*2, 20, 20, false, 0L),
-        BOOST  (1, "Nitro Booster", 60*60*24*7*4, 30, 25, true,  585981877396045826L),
-        PATRON (2, "Patron",        60*60*24*7*4, 30, 25, true,  585689274565918721L),
-        DONATOR(3, "Donator",       60*60*24*7*4, 30, 25, true,  585708901270421504L);
+        BOOST  (1, "Nitro Booster", 60*60*24*7*5, 30, 25, true,  585981877396045826L),
+        PATRON (2, "Patron",        60*60*24*7*5, 30, 25, true,  585689274565918721L),
+        DONATOR(3, "Donator",       60*60*24*7*5, 30, 25, true,  585708901270421504L);
         
         public final int level;
         public final String name;
